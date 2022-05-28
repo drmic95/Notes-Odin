142eea2232425669a1decc54f0d9e049946c7e1d
drmic95 <ante.drmic1b@gmail.com>
2022-05-28 09:59:51 +0200

    eventListener added

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ function startingGrid(num) {
 }
 
 // 4x4 as default grid
-// startingGrid(16);
+startingGrid(16);
 
 // 6x6 grid
 function firstGrid(num) {
@@ -20,7 +20,6 @@ function firstGrid(num) {
     newDiv.classList.add('firstGrid');
   }
 }
-
 // firstGrid(36);
 
 // 8x8 grid
@@ -41,3 +40,19 @@ function thirdGrid(num) {
   }
 }
 // thirdGrid(100);
+
+// addEventListener for btns
+
+mainSpanBtn.addEventListener('click', function (event) {
+  const first_grid = event.target.classList.contains('first_grid');
+  const second_grid = event.target.classList.contains('second_grid');
+  const third_grid = event.target.classList.contains('third_grid');
+
+  if (first_grid) {
+    firstGrid(36);
+  } else if (second_grid) {
+    secondGrid(64);
+  } else if (third_grid) {
+    thirdGrid(100);
+  } else console.log('something went wrong');
+});
